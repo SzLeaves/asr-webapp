4abef4f28a87f4b1e62adf3c6259fa0f7ef737cf
szhou <steven-zh@foxmail.com>
2023-04-26 00:18:45 +0800

    fix: asgi module load

--- asrlab/asgi.py
+++ asrlab/asgi.py
@@ -1,11 +1,14 @@
 import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'asrlab.settings')
+django.setup()
 
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
 from chatapp.routing import websocket_urlpatterns
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'asrlab.settings')
 
 # 支持http请求和websocket请求
 application = ProtocolTypeRouter({
